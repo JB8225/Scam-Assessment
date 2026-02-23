b8238ccad0eb28af1a936cbfa71fb07416f4b6a6
JB8225 <jb@onebuckcapital.com>
2026-02-23 14:21:41 -0600

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ScamEmergencyAssessment from './ScamEmergencyAssessment'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ScamEmergencyAssessment />
+  </React.StrictMode>
+)
